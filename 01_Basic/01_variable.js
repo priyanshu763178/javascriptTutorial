ed881a20a6cc5ad3c6a05512703fdd427ffce48d
priyanshu kumar <priyanshukumar805132@gmail.com>
2024-10-08 15:06:45 +0530

    knowledge about variables

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553;
+let accountEmail="anshhup95@gmail.com";
+var accountPassword ="12345";
+accountCity="Jaipur";
+// accountId= 12   //  not allowed
+
+// console.log(accountEmail);
+// console.log(accountId);
+// console.log(accountPassword);
+// console.log(accountCity)
+
+console.table([accountEmail,accountId,accountPassword,accountCity]);  // it is used to console all the log in the table
