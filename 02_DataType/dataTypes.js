c006694bc838b197b6833a9799b01c0620d0d887
priyanshu kumar <priyanshukumar805132@gmail.com>
2024-10-08 15:30:36 +0530

    data Types

---
+++ 02_DataType/dataTypes.js
@@ -0,0 +1,18 @@
+"use strict";  // treat all the code as newer version
+
+// alert(2+3);    // we are using nodejs not browser
+
+
+// string 
+//  numbr
+//  boolean
+// null 
+//  undefined
+//  symbol
+
+
+//  non-primitive data type 
+//  object 
+// array 
+console.log(typeof undefined);  // undefined
+console.log(typeof null)  // object 
